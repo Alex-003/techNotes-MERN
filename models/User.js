343556262d484d740a4b891a5932053b0a477b31
Alex Segura <62962671+Alex-003@users.noreply.github.com>
2023-05-19 18:35:37 +0100

    mongoose server installed

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: [
+    {
+      type: String,
+      default: "Employee",
+    },
+  ],
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
